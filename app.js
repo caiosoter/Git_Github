634313737fa668c16e679d87b62e4bf28998083c
caiosoter <caiosoter@hotmail.com>
2023-03-28 08:40:29 +0000

    Adicionando um novo console log

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está conectado ao banco de dados")
 console.log("Mais um glr")
+console.log("Enviando para o github")
