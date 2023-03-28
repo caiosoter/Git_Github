f1be35c78e78684007edb26abca5c1fd477f7cd4
caiosoter <caiosoter@hotmail.com>
2023-03-28 08:51:40 +0000

    restaurando

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está conectado ao banco de dados")
 console.log("Mais um glr")
-console.log("Enviando para o github")
